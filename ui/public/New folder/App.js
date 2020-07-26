9ba208ae5f488518a86317b0fec8c5d79eac2a9a
Bo Niu <niubo19930402@hotmail.com>
2020-07-26 06:04:50 -0700

    remove unnessary files

--- ui/public/New folder/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _IssueList = _interopRequireDefault(require("./IssueList.jsx"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/* globals React ReactDOM */
-var element = /*#__PURE__*/React.createElement(_IssueList.default, null);
-ReactDOM.render(element, document.getElementById('content'));
