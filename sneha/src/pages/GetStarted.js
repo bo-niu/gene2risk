6d3c805b1374e8aa8598fc09cfcb022aa30016ac
Sneha Ravichandran <sneha.rcd@gmail.com>
2020-08-16 18:36:33 -0700

    react router, css styling, and navbar component
    navbar component completed, the only page with content is the home page. the rest are just blank place holders for the other pages. they're setup using the react router to create multiple pages without having to reload the whole site

---
+++ sneha/src/pages/GetStarted.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function GetStarted() {
+  return (
+    <div className="GetStarted">
+    <Link to="/" className="cardlinks">Back</Link>
+      <h1>GetStarted</h1>
+    </div>
+  );
+}
+
+export default GetStarted;
